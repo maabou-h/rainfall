0ee262ed8b965c166af549c18526d90d26118e5c
maabou-h@student.42.fr <maabou-h@student.42.fr>
2020-07-31 12:19:23 +0200

    f

---
+++ level0/xploit.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int ac, char **av)
+{
+    if (atoi(av[1]) == 423)
+    {
+        char *shell = strdup("/bin/sh");
+        gid_t gid = getegid();
+        uid_t uid = geteuid();
+        char *args[2] = {
+            "sh",
+            0
+        };
+        execv(shell, args);
+    }
+    else
+        fwrite("No !\n", 1, 5, stderr);
+    return 0;
+}
---
+++ level1/xploit.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void run(void)
+{
+    fwrite("Good... Wait what?\n", 1, 19, stdout);
+    system("/bin/sh");
+}
+
+int main(int ac, char **av)
+{
+    gets(64);
+    return (0);
+}
